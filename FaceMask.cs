43ab5b925736edd7baf09b957a6858eb054ebe52
Kevinrkeith <46653751+Kevinrkeith@users.noreply.github.com>
2020-01-30 23:26:59 -0500

    Add files via upload

---
+++ FaceMask.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceMask : PowerUp
+{
+    public override void UsePowerUp()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            player.GetComponent<PlayerBody>().otherPlayers[i].GetComponent<PlayerBody>().playerCamera.transform.Rotate(0, 0 * Time.deltaTime, 180);
+            player.GetComponent<PlayerBody>().otherPlayers[i].GetComponent<PlayerBody>().flip = true;
+        }
+    }
+
+    protected override PowerUp GetPowerUp()
+    {
+        return this;
+    }
+
+    protected override PowerUpTypes GetType()
+    {
+        return type;
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        type = PowerUpTypes.MASK;
+    }
+}
---
+++ Flip.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : PowerUp
+{
+    public void Awake()
+    {
+        type = PowerUpTypes.FLIP;
+    }
+
+    protected override PowerUp GetPowerUp()
+    {
+        return this;
+    }
+
+    protected override PowerUpTypes GetType()
+    {
+        return type;
+    }
+
+    public override void UsePowerUp()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (player.GetComponent<PlayerBody>().otherPlayers[i].GetComponent<PlayerBody>().flip)
+            {
+                player.GetComponent<PlayerBody>().otherPlayers[i].GetComponent<PlayerBody>().playerCamera.transform.Rotate(0, 0 * Time.deltaTime, 180);
+                player.GetComponent<PlayerBody>().otherPlayers[i].GetComponent<PlayerBody>().flip = true;
+            }
+        }
+    }
+}
