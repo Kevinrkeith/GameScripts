28ad2029686b0ab5eb5237c70062656eb19386d5
Kevinrkeith <46653751+Kevinrkeith@users.noreply.github.com>
2020-02-03 08:27:05 -0500

    Add files via upload

---
+++ Bounce.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : PowerUp
+{
+    [SerializeField]
+    int timesToBounce;
+    public override void UsePowerUp(int playerUsed)
+    {
+        GameManager.gameManager.playerBody[playerUsed].bouncing=true;
+    }
+}
