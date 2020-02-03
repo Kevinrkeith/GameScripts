d15fee164c38028b16bb7dbdc1bda95896ca77a0
Kevinrkeith <46653751+Kevinrkeith@users.noreply.github.com>
2020-02-03 08:27:30 -0500

    Add files via upload

---
+++ Boost.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Boost : PowerUp
+{
+    [SerializeField]
+    float multiplier;
+    public override void UsePowerUp(int playerUsed)
+    {
+        GameManager.gameManager.playerBody[playerUsed].ApplyForce(GameManager.gameManager.player[playerUsed].transform.position, multiplier, ForceMode.Acceleration);
+    }
+}
